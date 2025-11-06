149c0c8a14e49f1cd8942f8a18953b286e3c37b4
raincoach2-bit <raincoach2@gmail.com>
2025-11-06 12:22:06 +0900

    Create App component with basic layout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h1>マルハン阿見店 スロットデータビューアー</h1>
+      <p>ここに台番号と機種名のデータを表示します。</p>
+    </div>
+  );
+}
+
+export default App;
